f286c7437b821f0a6a5d643978d94e9c28a5de37
cbotelho <cbotelho@algartelecom.com.br>
2023-05-04 14:49:40 -0300

    config do spring

---
+++ src/main/java/com/example/restComSpring1/RestComSpring1Application.java
@@ -0,0 +1,13 @@
+package com.example.restComSpring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestComSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestComSpring1Application.class, args);
+	}
+
+}
